9cd6619f6233553f7f4684f149fb0401d8a7371a
OskarNowogorski <85084078+OskarNowogorski@users.noreply.github.com>
2021-06-06 13:45:55 +0200

    Add files via upload

---
+++ stopwatch/ClassLibrary1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class Stopwatch
+    {
+        public bool isRunning;
+        public TimeSpan Elapsed { get; set; }
+        public DateTime StartTime { get; set; }
+        public void Start()
+        {
+            if (isRunning == true)
+            {
+                throw new NotSupportedException("StopwatchAlreadyStartedException");
+            } else           
+            {
+                StartTime = System.DateTime.Now - Elapsed;
+                isRunning = true;
+            }
+        }
+        public void Stop()
+        {
+            if (isRunning == false)
+            {
+                Console.WriteLine("The stopwatch is not running.");
+            } else           
+            {
+                Elapsed = System.DateTime.Now - StartTime;
+                isRunning = false;
+                Console.WriteLine($"Time elapsed: {Elapsed}");
+            }
+        }   
+        
+        public void Reset()
+        {
+            Elapsed = TimeSpan.Zero;
+            StartTime = System.DateTime.Now;           
+        }
+    }
+}
+        
---
+++ stopwatch/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using StopWatch;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var StopWatch = new StopWatch();
+            while (true)
+            {
+                Console.WriteLine("Enter a command start|stop|reset):");
+                string command = Console.ReadLine();
+
+                if (command == "start")
+                {
+                    StopWatch.Start();
+                } else if (command == "stop")
+                {
+                    StopWatch.Stop();                }
+                  else if (command == "reset")
+
+                {
+                    StopWatch.Reset();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+                
